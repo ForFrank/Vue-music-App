0ac0d8d7af772a799206eb4da149b7668b4eae98
frank <liangzhenqi@hotmail.com>
2020-08-02 10:24:36 +0800

    finish chapter1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <m-header></m-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MHeader from './components/m-header/m-header'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MHeader
   }
+ 
 }
 </script>
 
-<style>
+<style scoped lang="stylus" rel="stylesheet/stylus">
+@import "./common/stylus/variable";
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color:$color-theme
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
+import 'babel-polyfill'
 import Vue from 'vue'
 import App from './App.vue'
+import '../src/common/stylus/index.styl'
+import fastclick from 'fastclick'
 
 Vue.config.productionTip = false
+fastclick.attach(document.body)
 
 new Vue({
   render: h => h(App),
