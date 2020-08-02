bccf34b9aca022dbaca6ec8baa2a513dc9a4b1a4
frank <liangzhenqi@hotmail.com>
2020-08-02 16:00:48 +0800

    finish chapter2

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import '../src/common/stylus/index.styl'
 import fastclick from 'fastclick'
+import router from './router'
 
 Vue.config.productionTip = false
 fastclick.attach(document.body)
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Recommend from '../components/recommend/recommend'
+import Singer from '../components/singer/singer'
+import Tab from '../components/tab/tab'
+import Rank from '../components/rank/rank'
+
 Vue.use(Router)
+export default new Router({
+    routes: [
+        {
+            path: '/',
+            redirect: '/recommend'
+        },
+        {
+            path: '/recommend',
+            component: Recommend
+        },
+        {
+            path: '/singer',
+            component: Singer
+        },
+        {
+            path: '/tab',
+            component: Tab
+        },
+        {
+            path: '/rank',
+            component: Rank
+        }
+    ]
+})
