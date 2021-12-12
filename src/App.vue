b9a46f514fe6729d9edb81b9d897b4f7d9743c15
梁振奇 <liangzhenqi@hotmail.com>
2021-12-12 23:36:37 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
-<template>
-  <div id="app">
-    <m-header></m-header>
-    <tab></tab>
-    <router-view></router-view>
-  </div>
-</template>
-
-<script>
-import MHeader from './components/m-header/m-header'
-import Tab from './components/tab/tab'
-
-export default {
-  name: 'App',
-  components: {
-    MHeader,
-    Tab
-  }
- 
-}
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
 </script>
 
-<style scoped lang="stylus" rel="stylesheet/stylus">
-@import "./common/stylus/variable";
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+</template>
+
+<style>
 #app {
-  color:$color-theme
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,14 +1,4 @@
-import 'babel-polyfill'
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
-import '../src/common/stylus/index.styl'
-import fastclick from 'fastclick'
-import router from './router'
 
-Vue.config.productionTip = false
-fastclick.attach(document.body)
-
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+createApp(App).mount('#app')
