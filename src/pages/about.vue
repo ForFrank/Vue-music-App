0c8504dc01b88a344dcdf05586ca31bdade5a76c
梁振奇 <liangzhenqi@hotmail.com>
2021-12-14 23:34:05 +0800

    add chapter-8

---
+++ src/pages/about.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1 @click="toggle">click</h1>
+  {{x}},{{y}}
+  <button @click="loading">123</button>
+  <button @click="exit">exit</button>
+  <button @click="enter">enter</button>
+  <Rate :value="3" ></Rate>
+  <Rate :value="4" theme="red"></Rate>
+  <Rate :value="1" theme="green"></Rate>
+</template>
+<script setup>
+import { useFullscreen } from '@vueuse/core'
+import {useMouse} from '../utils/mouse'
+import useFavicon from '../utils/useFavicon' 
+import {ref} from 'vue'
+import Rate from '../components/Rate.vue'
+let score = ref(3)
+let {favicon}  = useFavicon() 
+function loading(){   
+    favicon.value = '../assets/test.png'
+}
+
+let {x,y} = useMouse()
+const { isFullscreen, enter, exit, toggle } = useFullscreen()
+</script>
---
+++ src/pages/home.vue
@@ -0,0 +1,9 @@
+<template>
+<h1>home</h1>
+<Count/>
+<Todolist />
+</template>
+<script setup>
+import Todolist from '../components/Todolist.vue'
+import Count from '../components/Count.vue'
+</script>
